122a20314a2ddf262f853bd88002c62e4b21b626
MaciekDul <maciej.dul@onet.pl>
2023-02-15 20:01:40 +0100

    a
    a

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,15 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsNew = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredient;
+  ingredientsNew.append(item);
+}
